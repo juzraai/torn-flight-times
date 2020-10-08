98a93527b22a2f0759f43a23d3110ddf65178606
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2020-10-09 00:20:31 +0200

    Complete rewrite, nicer design

--- torn-flight-times.js
+++ torn-flight-times.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Torn Flight Times
-// @version      1.0.0
+// @version      2.0.0
 // @description  Show flight times before flying
 // @author       juzraai
 // @license      MIT
@@ -13,32 +13,60 @@
 // ==/UserScript==
 
 /**
- * Adds real time clock values for landings (destination and Torn) for the
- * currently selected destination on Travel Agency screen.
+ * Format UNIX time in milliseconds to "HH:MM" string.
+ *
+ * @param {Number} ms time in milliseconds
+ * @return "HH:MM" string representation in current time zone
  */
-function addFlightTimeCalculator() {
-	const flightTime = document.querySelector('[aria-expanded=true] .travel-container .flight-time');
-	//if (!flightTime) return;
-	const hourMin = flightTime.textContent.split('-')[1].trim();
-	const [hour, minute] = hourMin.split(':').map(s => Number(s));
+function formatTime(ms) {
+	return new Date(ms).toTimeString().substring(0, 5);
+}
+
+/**
+ * Adds landing times below "Flight Time".
+ */
+function addFlightTimes() {
+
+	// This will be our new element:
+	let flightTimesEl = document.querySelector('[aria-expanded=true] .travel-container .flight-time .flight-times');
+
+	// If it doesn't exist:
+	if (!flightTimesEl) {
+
+		// Create it:
+		flightTimesEl = document.createElement('span');
+		flightTimesEl.classList.add('flight-times');
+		flightTimesEl.style.flexGrow = 1;
+
+		// Style the containers:
+		const flightTimeEl = document.querySelector('[aria-expanded=true] .travel-container .flight-time');
+		flightTimeEl.style.display = 'flex';
+		flightTimeEl.style.justifyContent = 'center';
+		flightTimeEl.style.flexWrap = 'wrap';
+		flightTimeEl.style.lineHeight = 1.2;
+
+		const travelInfoEl = document.querySelector('[aria-expanded=true] .travel-container .travel-info');
+		travelInfoEl.style.alignItems = 'center';
+		travelInfoEl.style.display = 'flex';
+
+		// And add it:
+		flightTimeEl.append(flightTimesEl);
+	}
+
+	// Extract "Flight Time":
+	const flightTimeStr = flightTimeEl.textContent.split('-')[1].trim().split(/\s/)[0];
+	const [hour, minute] = flightTimeStr.split(':').map(s => Number(s));
 	const flightTimeMs = (hour * 60 + minute) * 60 * 1000;
 
+	// Calculate landing times:
 	const now = new Date().getTime();
 	const landTimeMs = now + flightTimeMs;
 	const returnTimeMs = landTimeMs + flightTimeMs;
 
-	const landTime = new Date(landTimeMs);
-	const returnTime = new Date(returnTimeMs);
-
-	const landTimeFormatted = String(landTime.getHours()).padLeft(2, 0) + ':' + String(landTime.getMinutes()).padLeft(2, 0);
-	const returnTimeFormatted = String(returnTime.getHours()).padLeft(2, 0) + ':' + String(returnTime.getMinutes()).padLeft(2, 0);
-
-	const panel = document.querySelector('.travel-agency + .clear');
-	panel.innerHTML = `Land @ ~${landTimeFormatted} | Return @ ~${returnTimeFormatted}`;
-	panel.style.fontSize = '1.25rem';
-	panel.style.fontWeight = 'bold';
-	panel.style.padding = '1rem';
-	panel.style.textAlign = 'center';
+	// Display landing times:
+	flightTimesEl.innerHTML = `Landings - ${formatTime(landTimeMs)}, ${formatTime(returnTimeMs)}`
 }
 
-setInterval(addFlightTimeCalculator, 500);
+// Refreshing constantly:
+setInterval(addFlightTimes, 500); // This is how we react to when user selects another city or flying mode.
+// TODO Would be nice to do it more efficiently :)
